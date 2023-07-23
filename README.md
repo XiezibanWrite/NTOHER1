713aaa75203060f5f67dabd66e5e74ec14d04e32
LezaiYa <1792157206@qq.com>
2023-07-23 10:36:04 +0800

    上传1.1.1源码

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# NTOHER1
-A TOHE branch mod
+
+<h1 align="center">~ TownofHost Edited ~</h1>
+
+<br>
+
+<img align="left" alt="Cover" src="Resources/Images/TOHE-BG.jpg" width="50%" height="auto" /> 
+
+<p align="right">
+  
+> ### Regarding this mod
+>
+> This mod is not affiliated with Among Us or Innersloth LLC, and the content contained therein is not endorsed or otherwise sponsored by Innersloth LLC. Portions of the materials contained herein are property of Innersloth LLC. © Innersloth LLC.
+>
+
+<p align="center">
+<a href="https://tohe.cc" target="_blank"><img src="https://img.shields.io/badge/Website%20-%231DA1F2.svg?&style=for-the-badge&logo=vuedotjs&logoColor=white&color=3aa675"/></a>
+<a href="https://discord.gg/hkk2p9ggv4" target="_blank"><img src="https://img.shields.io/badge/Discord%20-%231DA1F2.svg?&style=for-the-badge&logo=discord&logoColor=white&color=5662f6"/></a>
+<a href="https://github.com/KARPED1EM/TownOfHostEdited/releases/latest" target="_blank"><img src="https://img.shields.io/badge/Lateset Version%20-%231DA1F2.svg?&style=for-the-badge&logo=github&logoColor=white&color=181717"/></a>
+</p>
+
+</p>
+
+---
+
+**This is a project for me to learn C#.**
+
+**So many thanks to these mods for their codes and helps:**
+
+> 
+### :star: [TOH](https://github.com/tukasa0001/TownOfHost) :
+> 
+> - Our repo is forked from TOH
+> 
+### :star: [TOHY](https://github.com/Yumenopai/TownOfHost_Y) :
+> 
+> - Many role ideas
+> - Provided role: AntiAdminer, CursedWolf, Workaholic, Greedier, DarkHide
+> - Reference: Modify game announcement
+> 
+### :star: [TOH:TOR](https://github.com/music-discussion/TownOfHost-TheOtherRoles) :
+> 
+> - Many role ideas
+> - Reference: Exile Confirm
+> - Reference: Split RPC Pack
+> 
+### :star: [SNR](https://github.com/ykundesu/SuperNewRoles) :
+> 
+> - Reference: Credentials menu
+> - Reference: Switch Horse Mode
+> - Reference: Search Mod Game
+> - Reference: Custom Button
+>
+### :star: [TOP](https://github.com/tugaru1975/TownOfPlus) :
+> 
+> - Reference: Zoom
+> 
+### :star: [RHR](https://github.com/sansaaaaai/Revolutionary-host-roles) :
+> 
+> - Reference: Modify settings menu
+> 
---
+++ Roles/Neutral/Jackal.cs
@@ -0,0 +1,64 @@
+using AmongUs.GameOptions;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using static TOHE.Options;
+
+namespace TOHE.Roles.Neutral;
+
+public static class Jackal
+{
+    private static readonly int Id = 50900;
+    public static List<byte> playerIdList = new();
+
+    private static OptionItem KillCooldown;
+    public static OptionItem CanVent;
+    public static OptionItem CanUseSabotage;
+    public static OptionItem CanWinBySabotageWhenNoImpAlive;
+    private static OptionItem HasImpostorVision;
+    private static OptionItem ResetKillCooldownWhenSbGetKilled;
+
+    public static void SetupCustomOption()
+    {
+        //Jackalは1人固定
+        SetupSingleRoleOptions(Id, TabGroup.NeutralRoles, CustomRoles.Jackal, 1, zeroOne: false);
+        KillCooldown = FloatOptionItem.Create(Id + 10, "KillCooldown", new(0f, 180f, 2.5f), 20f, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Jackal])
+            .SetValueFormat(OptionFormat.Seconds);
+        CanVent = BooleanOptionItem.Create(Id + 11, "CanVent", true, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Jackal]);
+        CanUseSabotage = BooleanOptionItem.Create(Id + 12, "CanUseSabotage", true, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Jackal]);
+        CanWinBySabotageWhenNoImpAlive = BooleanOptionItem.Create(Id + 14, "JackalCanWinBySabotageWhenNoImpAlive", true, TabGroup.NeutralRoles, false).SetParent(CanUseSabotage);
+        HasImpostorVision = BooleanOptionItem.Create(Id + 13, "ImpostorVision", true, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Jackal]);
+        ResetKillCooldownWhenSbGetKilled = BooleanOptionItem.Create(Id + 15, "ResetKillCooldownWhenPlayerGetKilled", false, TabGroup.NeutralRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Jackal]);
+    }
+    public static void Init()
+    {
+        playerIdList = new();
+    }
+    public static void Add(byte playerId)
+    {
+        playerIdList.Add(playerId);
+
+        if (!AmongUsClient.Instance.AmHost) return;
+        if (!Main.ResetCamPlayerList.Contains(playerId))
+            Main.ResetCamPlayerList.Add(playerId);
+    }
+    public static bool IsEnable => playerIdList.Count > 0;
+    public static void SetKillCooldown(byte id) => Main.AllPlayerKillCooldown[id] = KillCooldown.GetFloat();
+    public static void ApplyGameOptions(IGameOptions opt) => opt.SetVision(HasImpostorVision.GetBool());
+    public static void CanUseVent(PlayerControl player)
+    {
+        bool jackal_canUse = CanVent.GetBool();
+        DestroyableSingleton<HudManager>.Instance.ImpostorVentButton.ToggleVisible(jackal_canUse && !player.Data.IsDead);
+        player.Data.Role.CanVent = jackal_canUse;
+    }
+    public static void SetHudActive(HudManager __instance, bool isActive)
+    {
+        __instance.SabotageButton.ToggleVisible(isActive && CanUseSabotage.GetBool());
+    }
+    public static void AfterPlayerDiedTask(PlayerControl target)
+    {
+        var killer = target.GetRealKiller();
+        if (!ResetKillCooldownWhenSbGetKilled.GetBool() || killer == null) return;
+        Main.AllAlivePlayerControls.Where(x => x.PlayerId != killer.PlayerId && playerIdList.Contains(x.PlayerId)).Do(x => x.SetKillCooldown(0));
+    }
+}
